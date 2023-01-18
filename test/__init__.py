a9d01b89ef9711c81fae5cd631a14c4c439fb2e7
lucasrouge <lucas.rouge@hotmail.com>
2023-01-18 10:35:28 +0100

    Lucas caca

--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,73 @@
+#----------------------------------------------------------
+# File __init__.py
+#----------------------------------------------------------
+
+#    Addon info
+bl_info = {
+    'name': 'Multifile',
+    'author': 'Thomas Larsson',
+    'location': 'View3D > UI panel > Add meshes',
+    'category': '3D View'
+    }
+
+# To support reload properly, try to access a package var, 
+# if it's there, reload everything
+if "bpy" in locals():
+    import imp
+    imp.reload(mycube)
+    imp.reload(mysphere)
+    imp.reload(mycylinder)
+    print("Reloaded multifiles")
+else:
+    from . import mycube, mysphere, mycylinder
+    print("Imported multifiles")
+
 import bpy
-import sys
-from pathlib import Path
+from bpy.props import *
 
-sys.path.append(r"D:\Lucas\Documents\VIWD\Art\Freelance\2022\Blender to Nuke addon\Git\Exr2Nuke\test")
+#
+#   class AddMeshPanel(bpy.types.Panel):
+#
+class AddMeshPanel(bpy.types.Panel):
+    bl_label = "Add meshes"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "object"
 
-from script import test
- 
-resultat=script.test()
+    def draw(self, context):
+        self.layout.operator("multifile.add", 
+            text="Add cube").mesh = "cube"
+        self.layout.operator("multifile.add", 
+            text="Add cylinder").mesh = "cylinder"
+        self.layout.operator("multifile.add", 
+            text="Add sphere").mesh = "sphere"
+
+#
+#   class OBJECT_OT_AddButton(bpy.types.Operator):
+#
+class OBJECT_OT_AddButton(bpy.types.Operator):
+    bl_idname = "multifile.add"
+    bl_label = "Add"
+    mesh = bpy.props.StringProperty()
+
+    def execute(self, context):
+        if self.mesh == "cube":
+            mycube.makeMesh(-8)
+        elif self.mesh == "cylinder":
+            mycylinder.makeMesh(-5)
+        elif self.mesh == "sphere":
+            mysphere.makeMesh(-2)
+        return{'FINISHED'}    
+
+#
+#    Registration
+#
+
+def register():
+    bpy.utils.register_module(__name__)
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+if __name__ == "__main__":
+    register()
--- test/script.py
+++
@@ -1,9 +0,0 @@
-import bpy
-
-def test():
-    print("J'ai été loadé!")
-
-print(dir())
-
-if __name__ == "__main__":
-    register()
